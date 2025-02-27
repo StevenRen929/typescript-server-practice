b217c49ce4247a002911e739cf3839cea3bfad47
Steven Ren <stevenfrog@outlook.com>
2025-02-27 21:40:23 +1030

    delete useless code

--- app/routes/v1/api.ts
+++ app/routes/v1/api.ts
@@ -1,7 +1,7 @@
 import express, { Router } from 'express';
 import authController from '../../controllers/usersControllers/auth.controller';
 
-const router: Router = express.Router(); 
+const router= express.Router(); 
 
 router.post("/register", authController.registerController); 
 router.post("/login", authController.loginController); 
